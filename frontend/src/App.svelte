59e638c684bab3c1189c81753a251618156fb8bc
Jonny-Ponny <thebrivan@gmail.com>
2026-04-17 14:32:21 +0300

    Added 'Create folder in root' button to left panel header

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1126,6 +1126,29 @@
               </div>
             {/if}
 
+            <!-- Create folder in root button -->
+            <button
+              class="create-folder-btn"
+              onclick={() => handleCreateFolder("")}
+              title="Create folder in root"
+            >
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"
+                />
+                <line x1="12" y1="11" x2="12" y2="17" />
+                <line x1="9" y1="14" x2="15" y2="14" />
+              </svg>
+              <span>New Folder</span>
+            </button>
+
             <!-- Search input -->
             <div class="search-container">
               <input
